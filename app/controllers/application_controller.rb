dbd5bb757ca13e96da680d5ba69841ce4a6cfcb5
zumehasi1019 <zumehasi.takaya1019@gmail.com>
2017-09-14 16:35:18 +0900

    Deviseインストール〜サインアップ機能の追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,17 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+
+protect_from_forgery with: :exception
+before_action :configure_permitted_parameters, if: :devise_controller?
+
+def after_sign_in_path_for(resource)
+    root_path
+end
+
+private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+
 end
