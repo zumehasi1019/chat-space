a5bccd027f11c1a8dc6c4b27ff3f954b64de96db
zumehasi1019 <zumehasi.takaya1019@gmail.com>
2017-09-23 17:26:02 +0900

    コメント修正⑥

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,8 +3,8 @@ class MessagesController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @group = current_user.groups
-    @current_group =  @group.find(params[:group_id])
+    @groups = current_user.groups
+    @current_group =  @groups.find(params[:group_id])
     @message = @current_group.messages
     @current_message = Message.new
     @group_member = @current_group.users
