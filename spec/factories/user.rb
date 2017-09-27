4bbb8618b424dbb7a38e559e604255dbc2c8541a
zumehasi1019 <zumehasi.takaya1019@gmail.com>
2017-09-27 20:02:50 +0900

    コメント修正２

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :user do
     email     Faker::Internet.email
-    password "12345678"
+    password  Faker::Internet.password(8)
     name      Faker::Name.name
   end
 end
--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -2,38 +2,41 @@ require 'rails_helper'
 
 describe Message do
   describe '#create' do
+    context 'can save' do
+      it "is valid with a detail in the message " do
+        message = build(:message, image: nil)
+        expect(message).to be_valid
+      end
 
-    it "is valid with a detail in the message " do
-      message = build(:message, image: nil)
-      expect(message).to be_valid
+      it "is valid with a image in the message " do
+        message = build(:message)
+        expect(message).to be_valid
+      end
+
+      it "is valid with detail and image in the message" do
+        message = build(:message)
+        expect(message).to be_valid
+      end
     end
 
-    it "is valid with a image in the message " do
-      message = build(:message)
-      expect(message).to be_valid
-    end
+    context 'cannot save' do
+      it "is invalid without detail and image in the message" do
+        message = build(:message, detail: nil, image: nil)
+        message.valid?
+        expect(message).not_to be_valid
+      end
 
-    it "is valid with detail and image in the message" do
-      message = build(:message)
-      expect(message).to be_valid
-    end
+      it "is invalid without a group_id" do
+        message = build(:message, group_id: nil)
+        message.valid?
+        expect(message.errors[:group_id]).to include("を入力してください")
+      end
 
-    it "is invalid without detail and image in the message" do
-      message = build(:message, detail: nil, image: nil)
-      message.valid?
-      expect(message).not_to be_valid
-    end
-
-    it "is invalid without a group_id" do
-      message = build(:message, group_id: nil)
-      message.valid?
-      expect(message.errors[:group_id]).to include("を入力してください")
-    end
-
-    it "is invalid without a user_id" do
-      message = build(:message, user_id: nil)
-      message.valid?
-      expect(message.errors[:user_id]).to include("を入力してください")
+      it "is invalid without a user_id" do
+        message = build(:message, user_id: nil)
+        message.valid?
+        expect(message.errors[:user_id]).to include("を入力してください")
+      end
     end
   end
 end
