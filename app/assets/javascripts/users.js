45204db323eecdd0e42491e9a848df2ded58354c
zumehasi1019 <zumehasi.takaya1019@gmail.com>
2017-10-04 19:41:28 +0900

    削除・追加ボタンの実装

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,5 +1,8 @@
 $(function(){
   var search_list = $("#user-search-result");
+  var chat_member = $("#chat_member_js");
+  var remove_list = $(".chat-group-user__name")
+
   function appendListedUser(user) {
   var html = `<div class="chat-group-user clearfix">
                   <p class="chat-group-user__name"> ${ user.name } </p>
@@ -7,6 +10,19 @@ $(function(){
                 </div>`
     search_list.append(html);
   }
+  function appendAddedUser(id,name) {
+   var html = `<div class="chat-group-user clearfix id="chat-group-user-"${id}">
+               <input name="group[user_ids][]" type="hidden" value="${id}">
+                 <p class="chat-group-user__name"> ${ name } </p>
+                 <a class="user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn" data-user-id="${ id }", data-user-name= "${ name }">削除</a>
+               </div>`
+
+   chat_member.append(html);
+   $("#chat_member_js").on("click", ".chat-group-user__btn--remove", function() {
+    $(this).parent().remove();
+   });
+  }
+
   $("#user-search-field").on('keyup', function() {
     var input = $("#user-search-field").val();
     $.ajax({
@@ -29,5 +45,12 @@ $(function(){
      .fail(function() {
       alert('名前検索に失敗しました');
     })
+  });
+  $(".chat-group-form__field").on("click",".chat-group-user__btn", function() {
+    var id = $(this).data('user-id');
+    var name = $(this).data('user-name');
+    console.log(name);
+    appendAddedUser(id, name);
+    $(this).parent().remove();
   });
 });
